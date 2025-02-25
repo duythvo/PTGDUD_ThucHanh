8697ca2495cf1e7ec9a29a533fe401fa966f5a89
duythvo <vothaiduy19092004@gmail.com>
2025-02-25 20:55:18 +0700

    fix MyCompnents

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import MyComponents from "./components/MyComponents";
+import MyComponents from "./components/Component/MyComponents";
 import ToDoApp from "./components/ReactHooks/ToDoApp";
 import SinhVien from "./components/ReactHooks/SinhVien";
 
